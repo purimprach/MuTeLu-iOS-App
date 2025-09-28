2c5d9664cd72f550904b833038d4540ddd33bad0
purimprach <117452785+purimprach@users.noreply.github.com>
2025-09-28 19:49:28 +0700

    เพิ่มแบนเนอร์สีม่วง

--- MuTeLu29.swiftpm/RecommenderForYouView.swift
+++ MuTeLu29.swiftpm/RecommenderForYouView.swift
@@ -58,7 +58,7 @@ struct RecommenderForYouView: View {
                         flowManager.currentScreen = .editProfile
                     }
                 }
-                
+                DailyBannerView()
                 BuddhistDayBanner()
                 ReligiousHolidayBanner()
                 Spacer()
