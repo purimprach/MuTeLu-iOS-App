a6a866d3a82b09e533fa0a8bd66ec5882b687647
purimprach <117452785+purimprach@users.noreply.github.com>
2025-09-30 21:16:02 +0700

    ลบที่ไม่ใช้ทิ้ง

--- MuTeLu29-2.swiftpm/NumberMeaning.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct NumberMeaning: Codable, Identifiable {
-    var id: String { number }
-    let number: String
-    let meaning: String
-}
--- MuTeLu29-2.swiftpm/ShirtColor.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct ShirtColor: Codable {
-    let day: Int
-    let element: String
-    let shouldWear: [String]
-    let shouldAvoid: [String]
-    let canWear: [String]
-}
